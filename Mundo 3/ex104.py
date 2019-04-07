be32281abf88822f1f8ada03dcda8c2fbfe6c640
Douglas <douglas_dbp@hotmail.com>
2019-04-07 14:57:59 -0300

    mais cursos

---
+++ Mundo 3/ex104.py
@@ -0,0 +1,4 @@
+# Crie um programa que tenha a função leiaInt() do Python, só que fazendo validação para aceitar apenas um valor numérico.
+
+# Ex:
+n = leiaInt('Digite um n')
---
+++ Mundo 3/ex106.py
@@ -0,0 +1,2 @@
+# Faça um mini-sistema que utilize interative Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará.
+# OBS: use cores
